89c7a556edab52875574eab36caa30c526080ddf
Scott Munro <scottmunro@utexas.edu>
2015-09-01 23:30:45 -0500

    added placeholder functions for adding radio seeds to seed list and for generating radio

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,37 @@
-var testDataObj = function(artist, album, imgUrl) {
+'use strict;'
+
+var RadioSeed = function(artist, album, imgUrl) {
 	this.artist = artist;
 	this.album = album;
 	this.imgUrl = imgUrl;
 }
 
-var testData = [];
-for(var i = 0; i < 10; i++) {
-	testData.push(new testDataObj('artist_' + i, 'album_' + i,
-		'http://www.spirit-animals.com/wp-content/uploads/2013/08/Penguin-3-African-x.jpg'));
-}
+//var radioSeeds = [];
+// for(var i = 0; i < 10; i++) {
+// 	radioSeeds.push(new radioSeed('artist_' + i, 'album_' + i,
+// 		'http://www.spirit-animals.com/wp-content/uploads/2013/08/Penguin-3-African-x.jpg'));
+// }
 
 var RadioListModel = function() {
 	var self = this;
-	self.radioSeeds = ko.observableArray(testData);	
+	self.artistInput = ko.observable();
+	self.albumInput = ko.observable();
+	self.radioSeeds = ko.observableArray([]);
+	
+	self.processEntry = function() {
+		self.radioSeeds.push(new RadioSeed(self.artistInput(), self.albumInput(),
+			'http://www.spirit-animals.com/wp-content/uploads/2013/08/Penguin-3-African-x.jpg'));
+		self.artistInput('');
+		self.albumInput('');
+		console.log(self.radioSeeds);	
+	};
+	
+	self.generateRadio = function() {
+		console.log('Generating radio for...');
+		self.radioSeeds().forEach(function(seed){
+			console.log('%s - %s', seed.artist, seed.album);
+		});
+	}	
 };
 
 var model = new RadioListModel();
