950c179b14485f8e447a56dca87f3003f9509f4d
Scott Munro <scottmunro@utexas.edu>
2015-09-02 00:02:49 -0500

    minor css shenanigans still in place. need to vertically center my radio entry

--- scripts/app.js
+++ scripts/app.js
@@ -18,7 +18,15 @@ var RadioListModel = function() {
 	self.albumInput = ko.observable();
 	self.radioSeeds = ko.observableArray([]);
 	
-	self.processEntry = function() {
+	/**
+	 * add a seed from the input fields to the seedlist
+	 * pre: both input fields must be filled in
+	 */
+	self.addSeed = function() {
+		// ensure that both fields are filled in
+		if(!self.artistInput() || !self.albumInput()) {
+			return;
+		}
 		self.radioSeeds.push(new RadioSeed(self.artistInput(), self.albumInput(),
 			'http://www.spirit-animals.com/wp-content/uploads/2013/08/Penguin-3-African-x.jpg'));
 		self.artistInput('');
@@ -26,12 +34,24 @@ var RadioListModel = function() {
 		console.log(self.radioSeeds);	
 	};
 	
+	/**
+	 * remove a seed from the seed list
+	 * triggered from seed's delete button
+	 * @param seed The seed to remove
+	 */
+	self.removeSeed = function(seed) {
+		self.radioSeeds.remove(seed)
+	};
+	
+	/**
+	 * Take the seedlist and generate a radio-like playlist from it
+	 */
 	self.generateRadio = function() {
 		console.log('Generating radio for...');
 		self.radioSeeds().forEach(function(seed){
 			console.log('%s - %s', seed.artist, seed.album);
 		});
-	}	
+	};	
 };
 
 var model = new RadioListModel();
